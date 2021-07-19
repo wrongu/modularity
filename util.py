221ae1f2614e8cac0a4b6927a14edc2569ab9836
wrongu <richard.d.lange.13@dartmouth.edu>
2021-07-19 17:06:44 -0400

    oops - forgot important util function

--- util.py
+++ util.py
@@ -6,6 +6,11 @@ def is_iterable(some_var):
         return False
 
 
+def merge_dicts(a, b):
+    # Just wrapping this fancy-looking expression (Python 3.5+ only). https://stackoverflow.com/a/26853961
+    return {**a, **b}
+
+
 class asobject(object):
     def __init__(self, d:dict):
         self.__dict__ = dict(d.items())
