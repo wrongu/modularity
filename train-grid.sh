e5fe0672ab60977ff5c4e49f7452c467c409824a
wrongu <richard.d.lange.13@dartmouth.edu>
2021-08-31 09:39:56 -0400

    train-grid.sh: separate logs dir per set of models

--- train-grid.sh
+++ train-grid.sh
@@ -2,6 +2,7 @@
 
 PYTHON=/home/rlange/.venv/mytorch/bin/python
 ENV="CUBLAS_WORKSPACE_CONFIG=:4096:8"
+JOBS=30
 
 ######################
 ## MNIST SUPERVISED ##
@@ -15,13 +16,13 @@ L1VALS=(0.00001 0.00003162278 0.0001 0.0003162278 0.001 0.003162278 0.01 0.03162
 DROPVALS=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7)
 RUNS=({0..8})
 
-parallel --linebuffer --tag --jobs=30 $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs --device=auto --devices=$CUDA_DEVICES --l2={3} --run={4} \
+parallel --linebuffer --tag --jobs=$JOBS $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-l2 --device=auto --devices=$CUDA_DEVICES --l2={3} --run={4} \
   ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L2VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=30 $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs --device=auto --devices=$CUDA_DEVICES --l1={3} --run={4} \
+parallel --linebuffer --tag --jobs=$JOBS $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-l1 --device=auto --devices=$CUDA_DEVICES --l1={3} --run={4} \
   ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L1VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=30 $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs --device=auto --devices=$CUDA_DEVICES --drop={3} --run={4} \
+parallel --linebuffer --tag --jobs=$JOBS $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-drop --device=auto --devices=$CUDA_DEVICES --drop={3} --run={4} \
   ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${DROPVALS[@]} ::: ${RUNS[@]}
 
 ########################
@@ -36,12 +37,12 @@ L1VALS=(0.0000001 0.0000003162278 0.000001 0.000003162278 0.00001 0.00003162278 
 DROPVALS=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7)
 RUNS=({0..5})
 
-parallel --linebuffer --tag --jobs=30 $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs --device=auto --devices=$CUDA_DEVICES --l2={3} --run={4} \
+parallel --linebuffer --tag --jobs=$JOBS $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-l2 --device=auto --devices=$CUDA_DEVICES --l2={3} --run={4} \
   ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L2VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=30 $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs --device=auto --devices=$CUDA_DEVICES --l2=0.0000001 --l1={3} --run={4} \
+parallel --linebuffer --tag --jobs=$JOBS $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-l1 --device=auto --devices=$CUDA_DEVICES --l2=0.0000001 --l1={3} --run={4} \
   ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L1VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=30 $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs --device=auto --devices=$CUDA_DEVICES --l2=0.0000001 --drop={3} --run={4} \
+parallel --linebuffer --tag --jobs=$JOBS $ENV $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-drop --device=auto --devices=$CUDA_DEVICES --l2=0.0000001 --drop={3} --run={4} \
   ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${DROPVALS[@]} ::: ${RUNS[@]}
 
