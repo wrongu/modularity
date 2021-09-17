db0f9f388f6c9c2f717058157de7ede5fd6f3424
wrongu <richard.d.lange.13@dartmouth.edu>
2021-09-17 09:33:12 -0400

    jacobian-based association methods

--- associations.py
+++ associations.py
@@ -29,13 +29,30 @@ def sum_hessian(loss, hidden):
     return hessian
 
 
+def batch_jacobian(h1, h2):
+    """Get batch-wise jacobians of h2 with respect to h1. Both must have size (b, ?), and output will be of size
+    (b, |h1|, |h2|)
+
+    Assuming computation graph has no dependence of h2[i,...] on h1[j,...] unless i==j. This allows us to pass a lot
+    of vectorization over to torch by getting grad of h2.sum() w.r.t. h1
+    """
+    b = h1.size(0)
+    h2 = h2.reshape(b, -1)
+    d1, d2 = h1.numel()//b, h2.size(1)
+    jacobians = h1.new_zeros(b, d1, d2)
+    for i in range(d2):
+        jac_part = torch.autograd.grad(h2[:, i].sum(), h1, retain_graph=True)[0]
+        jacobians[:, :, i] = jac_part.reshape(b, d1)
+    return jacobians
+
+
 # TODO - include covariance in feature space (HSIC) for both fwd and bwd methods
 # TODO - include 'online' methods
-def get_associations(model, method, dataset, device='cpu', batch_size=200, shuffle=False):
+def get_associations(model, method, dataset, device='cpu', batch_size=200, shuffle=True):
     model.eval()
     model.to(device)
 
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, pin_memory=True)
     num_batches = ceil(len(dataset)/batch_size)
 
     if method in ['forward_cov', 'forward_cov_norm']:
@@ -62,8 +79,26 @@ def get_associations(model, method, dataset, device='cpu', batch_size=200, shuff
         # Ensure symmetry, since hessians will not be *exactly* symmetric up to floating point errors, and we will
         # be asserting symmetry later.
         assoc = [(a + a.T)/2. for a in assoc]
+    elif method in ['forward_jac', 'forward_jac_norm']:
+        assoc = [torch.zeros(d, d, device=device) for d in model.hidden_dims]
+        for im, la in tqdm(loader, desc=method, total=num_batches):
+            im, la = im.to(device), la.to(device)
+            im.requires_grad_(True)
+            hidden, out = model(im)
+            for a, h in zip(assoc, hidden):
+                jac = batch_jacobian(im, h)
+                a += torch.einsum('bix,biy->xy', jac, jac) / len(dataset)
+    elif method in ['backward_jac', 'backward_jac_norm']:
+        assoc = [torch.zeros(d, d, device=device) for d in model.hidden_dims]
+        for im, la in tqdm(loader, desc=method, total=num_batches):
+            im, la = im.to(device), la.to(device)
+            hidden, out = model(im)
+            for a, h in zip(assoc, hidden):
+                jac = batch_jacobian(h, out)
+                a += torch.einsum('bxi,byi->xy', jac, jac) / len(dataset)
     else:
-        allowed_methods = ['forward_cov', 'forward_cov_norm', 'backward_hess', 'backward_hess_norm']
+        allowed_methods = ['forward_cov', 'forward_cov_norm', 'backward_hess', 'backward_hess_norm',
+                           'forward_jac', 'forward_jac_norm', 'backward_jac', 'backward_jac_norm']
         raise ValueError(f"get_association requires method to be one of {allowed_methods}")
 
     if 'norm' in method:
