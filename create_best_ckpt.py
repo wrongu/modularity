b94d4283fa36862683751ca2211cda835d65296e
wrongu <richard.d.lange.13@dartmouth.edu>
2022-04-28 16:53:41 -0400

    verbose option in create_best_ckpt

--- create_best_ckpt.py
+++ create_best_ckpt.py
@@ -11,7 +11,8 @@ def bestify(weights_dir: Union[str, Path],
             field: str = "val_loss",
             mode: str = "min",
             overwrite: bool = False,
-            data_dir: Union[str, Path] = Path("data")):
+            data_dir: Union[str, Path] = Path("data"),
+            verbose: bool = False):
     weights_dir = Path(weights_dir)
     data_dir = Path(data_dir)
     best_file = weights_dir / "best.ckpt"
@@ -26,6 +27,8 @@ def bestify(weights_dir: Union[str, Path],
         evaluate(ckpt, data_dir, metrics=[field])
         data = torch.load(ckpt, map_location="cpu")
         val = data[field]
+        if verbose:
+            print(f"\t{ckpt}: {field}={val}")
         if (mode == "max" and val > best_val) or (mode == "min" and val < best_val):
             best_ckpt, best_val = ckpt.resolve(), val
 
@@ -34,20 +37,24 @@ def bestify(weights_dir: Union[str, Path],
         return
 
     # Only raise error if 'overwrite' is False and the 'best.ckpt' referee would change.
-    if best_file .exists():
-        previous_best = best_file .resolve()
+    if best_file.exists():
+        previous_best = best_file.resolve()
         if previous_best != best_ckpt and not overwrite:
             raise FileExistsError()
         elif previous_best == best_ckpt:
+            if verbose:
+                print(f"Keeping previous best for {weights_dir}: {best_ckpt}")
             return
 
     # If we reach here, it's time to create a new symlink called "best.ckpt" that points to the best_ckpt file
-    best_file .symlink_to(target=best_ckpt, target_is_directory=False)
+    if verbose:
+        print(f"Best for {weights_dir} is {best_ckpt}; creating link in {best_file}")
+    best_file.symlink_to(target=best_ckpt, target_is_directory=False)
 
 
 def main(args):
     assert args.directory.is_dir(), f"{args.directory} is not a directory"
-    bestify(args.directory, args.field, args.mode, args.overwrite)
+    bestify(args.directory, args.field, args.mode, args.overwrite, verbose=args.verbose)
     if args.recurse:
         for sub in args.directory.iterdir():
             if sub.is_dir():
@@ -62,6 +69,7 @@ if __name__ == "__main__":
     parser.add_argument("--mode", default="min", type=str, choices=["min", "max"])
     parser.add_argument("--recurse", action="store_true", default=False)
     parser.add_argument("--overwrite", action="store_true", default=False)
+    parser.add_argument("--verbose", action="store_true", default=False)
     args = parser.parse_args()
 
     main(args)
