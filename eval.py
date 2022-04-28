c6fde59a8ff33d47ca9650868a4d7ff1b06ac79c
wrongu <richard.d.lange.13@dartmouth.edu>
2022-04-28 16:53:10 -0400

    QoL improvements in eval.py, namely loading a model only if eval needed

--- eval.py
+++ eval.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from models import LitWrapper
 from associations import get_similarity_by_layer, get_similarity_combined
+from associations import METHODS as association_methods
 from modularity import monte_carlo_modularity, girvan_newman, soft_num_clusters, is_valid_adjacency_matrix, \
     alignment_score, shuffled_alignment_score, sparsify, cluster_id
 from torch.utils.data import DataLoader
@@ -10,6 +11,7 @@ from clusim.clustering import Clustering
 import clusim.sim as sim
 from warnings import warn
 from tqdm import tqdm
+from functools import lru_cache
 
 
 __MOD_VERSION = 3
@@ -46,11 +48,16 @@ def accuracy(mdl, dataset, task, topk=1, device='cpu'):
     return acc.item() / len(dataset)
 
 
+@lru_cache(maxsize=1)
+def load_model_once(checkpoint_file, data_dir, device):
+    model = LitWrapper.load_from_checkpoint(checkpoint_file, map_location=device)
+    # TODO - include a dataset that's just noise as a kind of reference/null
+    data_train, data_val, data_test = model.get_dataset(data_dir)
+    return model.to(device), data_train, data_val, data_test
+
+
 def evaluate(checkpoint_file, data_dir, metrics=None, device='cpu'):
     info = torch.load(checkpoint_file)
-    model = LitWrapper.load_from_checkpoint(checkpoint_file)
-    model.to(device)  # TODO - why didn't load_from_checkpoint(map_location=device) work?
-    data_train, data_val, data_test = model.get_dataset(data_dir)
 
     if metrics is None:
         metrics = ['train_loss',
@@ -66,26 +73,36 @@ def evaluate(checkpoint_file, data_dir, metrics=None, device='cpu'):
 
     # Loss and accuracy metrics
     if 'train_loss' not in info and 'train_loss' in metrics:
-        info['train_loss'] = loss(model, data_train, model.hparams.task, device=device)
+        model, data_train, _, _ = load_model_once(checkpoint_file, data_dir, device)
+        info['train_loss'] = loss(model, data_train, 'sup', device=device)
     if 'train_acc' not in info and 'train_acc' in metrics:
-        info['train_acc'] = accuracy(model, data_train, model.hparams.task, device=device)
+        model, data_train, _, _ = load_model_once(checkpoint_file, data_dir, device)
+        info['train_acc'] = accuracy(model, data_train, 'sup', device=device)
     if 'val_loss' not in info and 'val_loss' in metrics:
-        info['val_loss'] = loss(model, data_val, model.hparams.task, device=device)
+        model, _, data_val, _ = load_model_once(checkpoint_file, data_dir, device)
+        info['val_loss'] = loss(model, data_val, 'sup', device=device)
     if 'val_acc' not in info and 'val_acc' in metrics:
-        info['val_acc'] = accuracy(model, data_val, model.hparams.task, device=device)
+        model, _, data_val, _ = load_model_once(checkpoint_file, data_dir, device)
+        info['val_acc'] = accuracy(model, data_val, 'sup', device=device)
     if 'test_loss' not in info and 'test_loss' in metrics:
-        info['test_loss'] = loss(model, data_test, model.hparams.task, device=device)
+        model, _, _, data_test = load_model_once(checkpoint_file, data_dir, device)
+        info['test_loss'] = loss(model, data_test, 'sup', device=device)
     if 'test_acc' not in info and 'test_acc' in metrics:
-        info['test_acc'] = accuracy(model, data_test, model.hparams.task, device=device)
+        model, _, _, data_test = load_model_once(checkpoint_file, data_dir, device)
+        info['test_acc'] = accuracy(model, data_test, 'sup', device=device)
 
     # Weight norms, using LitWrapper.l2_norm and LitWrapper.l1_norm
     if 'l2_norm' not in info and 'l2_norm' in metrics:
+        model, _, _, _ = load_model_once(checkpoint_file, data_dir, device)
         info['l2_norm'] = model.l2_norm().item()
     if 'l1_norm' not in info and 'l1_norm' in metrics:
+        model, _, _, _ = load_model_once(checkpoint_file, data_dir, device)
         info['l1_norm'] = model.l1_norm().item()
     if 'sparsity' not in info and 'sparsity' in metrics:
+        model, _, _, _ = load_model_once(checkpoint_file, data_dir, device)
         info['sparsity'] = model.sparsity().item()
     if 'nuc_norm' not in info and 'nuc_norm' in metrics:
+        model, _, _, _ = load_model_once(checkpoint_file, data_dir, device)
         info['nuc_norm'] = model.nuc_norm().item()
 
     torch.save(info, checkpoint_file)
@@ -94,9 +111,6 @@ def evaluate(checkpoint_file, data_dir, metrics=None, device='cpu'):
 
 def eval_modularity(checkpoint_file, data_dir, target_entropy=None, mc_steps=5000, metrics=None, sparseness=None, align=True, combined=False, device='cpu'):
     info = torch.load(checkpoint_file)
-    model = LitWrapper.load_from_checkpoint(checkpoint_file, map_location=device)
-    # TODO - include a dataset that's just noise as a kind of reference/null
-    _, _, data_test = model.get_dataset(data_dir)
 
     # Use defaults unless target_entropy is given
     mc_kwargs = {'device': device}
@@ -109,8 +123,7 @@ def eval_modularity(checkpoint_file, data_dir, target_entropy=None, mc_steps=500
         sparseness = [None] + list(sparseness)
 
     if metrics is None:
-        metrics = ['forward_cov', 'forward_cov_norm', 'backward_hess', 'backward_hess_norm',
-                   'forward_jac', 'forward_jac_norm', 'backward_jac', 'backward_jac_norm']
+        metrics = association_methods
 
     # Precompute 'association' matrices and store in 'assoc' dictionary of checkpoint data
     assoc_info = info.get('assoc', {})
@@ -119,7 +132,8 @@ def eval_modularity(checkpoint_file, data_dir, target_entropy=None, mc_steps=500
         # of similarity matrices, one per layer
         if meth not in assoc_info:
             # compute layer-wise association info
-            assoc_info[meth] = get_similarity_by_layer(model, meth, data_test, device=device)
+            model, _, _, data_test = load_model_once(checkpoint_file, data_dir, device)
+            assoc_info[meth] = get_similarity_by_layer(model, meth, data_test, device=device, batch_size=100)
             if not all(is_valid_adjacency_matrix(m, enforce_sym=True) for m in assoc_info[meth]):
                 warn(f"First sanity check on association method {meth} failed!")
 
@@ -129,6 +143,7 @@ def eval_modularity(checkpoint_file, data_dir, target_entropy=None, mc_steps=500
             combo_key = meth + '_combined'
             if combo_key not in assoc_info:
                 # compute combined association info
+                model, _, _, data_test = load_model_once(checkpoint_file, data_dir, device)
                 assoc_info[combo_key] = [get_similarity_combined(model, meth, data_test, device=device)]
                 if not is_valid_adjacency_matrix(assoc_info[combo_key][0], enforce_sym=True):
                     warn(f"First sanity check on association method {combo_key} failed!")
@@ -138,7 +153,8 @@ def eval_modularity(checkpoint_file, data_dir, target_entropy=None, mc_steps=500
     # For each requested method, compute and store (1) cluster assignments and (2) modularity score
     module_info = info.get('modules', {})
     for imeth in range(len(metrics)):
-        for suffix in ['', '_combined']:
+        suffixes = ['', '_combined'] if combined else ['']
+        for suffix in suffixes:
             meth = metrics[imeth] + suffix
             for sp in sparseness:
                 key = meth if sp is None else f"{meth}.{sp:.2f}"
