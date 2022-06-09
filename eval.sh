2d231a593689d346728f72d118fd41675faa8352
wrongu <richard.d.lange.13@dartmouth.edu>
2022-06-09 14:30:03 -0400

    Quality of life improvements to shells cripts eval.sh and train-grid.sh

--- eval.sh
+++ eval.sh
@@ -6,15 +6,16 @@ conda activate modularity
 
 echo "Using" `which python`
 
-CUDA_DEVICES=(0 1 2 3)
-JOBS=16
+CUDA_DEVICES=(1 2 3)
+JOBS=12
 
 ####################################
 ## BASIC STATS ON ALL CHECKPOINTS ##
 ####################################
 
 # This first pass over all .ckpt files computes stats like val_loss among others
-FILES=($(find $1 -name "*.ckpt" | grep -v "best.ckpt" | sort))
+FILES=($(find $1* -name "*.ckpt" | grep -v "best.ckpt" | grep -v "dummy.ckpt" | sort))
+echo "=== BASIC STATS ON ALL ${#FILES[@]} CHECKPOINTS ==="
 parallel --linebuffer --tag --link --jobs=$JOBS \
     python -m eval --ckpt-file={1} --device=cuda:{2} --skip-modularity \
     ::: ${FILES[@]} ::: ${CUDA_DEVICES[@]}
@@ -24,14 +25,19 @@ parallel --linebuffer --tag --link --jobs=$JOBS \
 ##########################################
 
 # Create a 'best.ckpt' alias for whichever checkpoint has lowest validation loss, per model
-python -m create_best_ckpt $1 --recurse --field=val_loss --mode=min
+echo "=== IDENTIFY BEST CHECKPOINT PER RUN ==="
+CKPT_DIRS=($(find $1* -type d | grep weights | grep -v dummy | sort))
+parallel --linebuffer --jobs=$JOBS \
+    python -m create_best_ckpt {1} --recurse --field=val_loss --mode=min \
+    ::: ${CKPT_DIRS[@]}
 
 ##########################################
 ## FANCY STATS ON BEST CHECKPOINTS ONLY ##
 ##########################################
 
 # Do modularity metrics only on the "best" checkpoint per model
-FILES=($(find $1 -name "best.ckpt" | sort))
+FILES=($(find $1* -name "best.ckpt" | sort))
+echo "=== RUN MODULARITY METRICS ON ${#FILES[@]} CHECKPOINTS ==="
 MODMETRICS="forward_cov,backward_hess,forward_jac,backward_jac,forward_cov_norm,backward_hess_norm,forward_jac_norm,backward_jac_norm"
 parallel --linebuffer --tag --link --jobs=$JOBS python -m eval --ckpt-file={1} --device=cuda:{2} \
   --modularity-metrics=$MODMETRICS ::: ${FILES[@]} ::: ${CUDA_DEVICES[@]}
@@ -40,7 +46,7 @@ parallel --linebuffer --tag --link --jobs=$JOBS python -m eval --ckpt-file={1} -
 ## STATS ON DUMMY CHECKPOINTS (UNTRAINED MODELS) ##
 ###################################################
 
-FILES=($(find $1 -name "dummy.ckpt" | sort))
-MODMETRICS="forward_cov,backward_hess,forward_jac,backward_jac,forward_cov_norm,backward_hess_norm,forward_jac_norm,backward_jac_norm"
+FILES=($(find $1* -name "dummy.ckpt" | sort | head -n 100))
+echo "=== RUN MODULARITY METRICS ON ${#FILES[@]} DUMMY CHECKPOINTS ==="
 parallel --linebuffer --tag --link --jobs=$JOBS python -m eval --ckpt-file={1} --device=cuda:{2} \
   --modularity-metrics=$MODMETRICS ::: ${FILES[@]} ::: ${CUDA_DEVICES[@]}
--- train-grid.sh
+++ train-grid.sh
@@ -2,45 +2,90 @@
 
 PYTHON=/home/rlange/anaconda3/envs/modularity/bin/python
 JOBS=9
+CUDA_DEVICES=123
 
-######################
-## MNIST SUPERVISED ##
-######################
+################################
+### MNIST BASE - EXPERIMENT 1 ##
+################################
 
-CUDA_DEVICES=123 # cuda:0 on dolores is different hardware, which doesn't guarantee deterministic results!
-DATASETS=(mnist)
-TASKS=(sup)
 L2VALS=(0.00001 0.00003162278 0.0001 0.0003162278 0.001 0.003162278 0.01 0.03162278 0.1)
-L1VALS=(0.00001 0.00003162278 0.0001 0.0003162278 0.001 0.003162278 0.01 0.03162278 0.1)
-DROPVALS=(0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7)
-RUNS=({0..8})
+L1VALS=(0.00001 0.00003162278 0.0001 0.0003162278 0.001 0.003162278 0.01)
+DROPVALS=(0.1 0.2 0.3 0.4 0.5 0.6 0.7)
+RUNS=({0..4})
 
-parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-l2 --device=auto --devices=$CUDA_DEVICES --l2={3} --run={4} \
-  ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L2VALS[@]} ::: ${RUNS[@]}
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup \
+  --save-dir=logs-mnist-base-l2 --device=auto --devices=$CUDA_DEVICES --l2={1} --run={2} \
+  ::: ${L2VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-l1 --device=auto --devices=$CUDA_DEVICES --l1={3} --run={4} \
-  ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L1VALS[@]} ::: ${RUNS[@]}
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup \
+  --save-dir=logs-mnist-base-l1 --device=auto --devices=$CUDA_DEVICES --l1={1} --run={2} \
+  ::: ${L1VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{2}-drop --device=auto --devices=$CUDA_DEVICES --drop={3} --run={4} \
-  ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${DROPVALS[@]} ::: ${RUNS[@]}
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup \
+  --save-dir=logs-mnist-base-drop --device=auto --devices=$CUDA_DEVICES --drop={1} --run={2} \
+  ::: ${DROPVALS[@]} ::: ${RUNS[@]}
 
-########################
-## CIFAR10 SUPERVISED ##
-########################
+###############################
+## MNIST WIDE - EXPERIMENT 2 ##
+###############################
 
-CUDA_DEVICES=123 # cuda:0 on dolores is different hardware, which doesn't guarantee deterministic results!
-DATASETS=(cifar10)
-TASKS=(sup)
+EXTRA_ARGS="{'channels':(256,256)}"
+L2VALS=(0.00001 0.0001 0.001 0.01 0.1)
+L1VALS=(0.00001 0.0001 0.001 0.01 0.1)
+DROPVALS=(0.1 0.2 0.3 0.4 0.5 0.6)
+RUNS=({0..2})
+
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup --model-args=\"${EXTRA_ARGS}\" \
+  --save-dir=logs-mnist-wide-l2 --device=auto --devices=$CUDA_DEVICES --l2={1} --run={2} \
+  ::: ${L2VALS[@]} ::: ${RUNS[@]}
+
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup --model-args=\"${EXTRA_ARGS}\" \
+  --save-dir=logs-mnist-wide-l1 --device=auto --devices=$CUDA_DEVICES --l1={1} --run={2} \
+  ::: ${L1VALS[@]} ::: ${RUNS[@]}
+
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup --model-args=\"${EXTRA_ARGS}\" \
+  --save-dir=logs-mnist-wide-drop --device=auto --devices=$CUDA_DEVICES --drop={1} --run={2} \
+  ::: ${DROPVALS[@]} ::: ${RUNS[@]}
+
+###############################
+## MNIST DEEP - EXPERIMENT 3 ##
+###############################
+
+EXTRA_ARGS="{'channels':(64,64,64,64,64)}"
+L2VALS=(0.00001 0.0001 0.001 0.01 0.1)
+L1VALS=(0.00001 0.0001 0.001 0.01 0.1)
+DROPVALS=(0.1 0.2 0.3 0.4 0.5 0.6)
+RUNS=({0..2})
+
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup --model-args=\"${EXTRA_ARGS}\" \
+  --save-dir=logs-mnist-deep-l2 --device=auto --devices=$CUDA_DEVICES --l2={1} --run={2} \
+  ::: ${L2VALS[@]} ::: ${RUNS[@]}
+
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup --model-args=\"${EXTRA_ARGS}\" \
+  --save-dir=logs-mnist-deep-l1 --device=auto --devices=$CUDA_DEVICES --l1={1} --run={2} \
+  ::: ${L1VALS[@]} ::: ${RUNS[@]}
+
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=mnist --task=sup --model-args=\"${EXTRA_ARGS}\" \
+  --save-dir=logs-mnist-deep-drop --device=auto --devices=$CUDA_DEVICES --drop={1} --run={2} \
+  ::: ${DROPVALS[@]} ::: ${RUNS[@]}
+
+##############
+### CIFAR10 ##
+##############
+
 L2VALS=(0.00001 0.0001 0.001 0.01 0.1)
 L1VALS=(0.00001 0.0001 0.001 0.01)
 DROPVALS=(0.1 0.2 0.3 0.4 0.5)
 RUNS=({0..2})
 
-parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{1}-{2}-l2 --device=auto --devices=$CUDA_DEVICES --l2={3} --run={4} \
-  ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L2VALS[@]} ::: ${RUNS[@]}
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=cifar10 --task=sup \
+  --save-dir=logs-cifar10-l2 --device=auto --devices=$CUDA_DEVICES --l2={1} --run={2} \
+  ::: ${L2VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{1}-{2}-l1 --device=auto --devices=$CUDA_DEVICES --l1={3} --run={4} \
-  ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${L1VALS[@]} ::: ${RUNS[@]}
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=cifar10 --task=sup \
+  --save-dir=logs-cifar10-l1 --device=auto --devices=$CUDA_DEVICES --l1={1} --run={2} \
+  ::: ${L1VALS[@]} ::: ${RUNS[@]}
 
-parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset={1} --task={2} --save-dir=logs-{1}-{2}-drop --device=auto --devices=$CUDA_DEVICES --drop={3} --run={4} \
-  ::: ${DATASETS[@]} ::: ${TASKS[@]} ::: ${DROPVALS[@]} ::: ${RUNS[@]}
+parallel --linebuffer --tag --jobs=$JOBS $PYTHON -m train --dataset=cifar10 --task=sup \
+  --save-dir=logs-cifar10-drop --device=auto --devices=$CUDA_DEVICES --drop={1} --run={2} \
+  ::: ${DROPVALS[@]} ::: ${RUNS[@]}
